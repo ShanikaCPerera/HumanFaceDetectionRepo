1f5692d6c2c45c4e57b808d9e9da3f0921b8c3bd
Shanika Perera <shanikachp@gmail.com>
2023-03-18 16:29:55 -0500

    Added an algorithm to remove duplicate  bounding boxes
    Added an algorithm to remove duplicate  bounding boxes

--- rcnn/util.py
+++ rcnn/util.py
@@ -36,6 +36,59 @@ def calculate_iou(bb1, bb2):
     assert iou <= 1.0
     return iou
 
+def non_max_suppression_fast(boxes, overlapThresh):
+    """
+    Remove overlapping bounding boxes that refer to the same object
+    in the input aray of bounding boxes.
+    Malisiewicz et al.
+    """
+
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+    # if the bounding boxes integers, convert them to floats
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+
+    pick = []
+
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+
+    while len(idxs) > 0:
+        # add the last index in the indexes list
+        # to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+                                               np.where(overlap > overlapThresh)[0])))
+
+    return boxes[pick].astype("int")
+
 class MyLabelBinarizer(LabelBinarizer):
     """
     Class that represents a Label Binarizer
